e4f645fd1245393b069a00fb74e088e25a67b2ec
MarkoMilovanovicYmagis <marko.milovanovic@ymagis.com>
2019-08-20 09:39:19 +0200

    docerization

--- src/main/java/com/ymagis/emulator/appliancestmp/ApplianceStmpApplication.java
+++ src/main/java/com/ymagis/emulator/appliancestmp/ApplianceStmpApplication.java
@@ -2,18 +2,19 @@ package com.ymagis.emulator.appliancestmp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplianceStmpApplication {
+public class ApplianceStmpApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplianceStmpApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApplianceStmpApplication.class, args);
-        try {
-            System.in.read();
-        } catch (IOException e) {
-        }
     }
 
 }
